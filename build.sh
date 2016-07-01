ba35dd5ff7f70fcb5acd369553e199518d60150e
Layne Peng <appamail@hotmail.com>
2016-07-02 00:48:55 +0800

    Remove the self links in Godeps, and use the soft link in build.sh. The shortage is this way only work in Linux/Mac.

--- build.sh
+++ build.sh
@@ -5,4 +5,9 @@ set -x
 
 export GOPATH=`pwd`
 
+if [ ! -d ./src/github.com/LaynePeng/ ]; then
+	mkdir -p ./src/github.com/LaynePeng
+	ln -s `pwd` ./src/github.com/LaynePeng
+fi
+
 GOARCH=amd64 GOOS=linux go build -o cfd
